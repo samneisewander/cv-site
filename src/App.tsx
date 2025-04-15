aa2f8e35eecb5d2d68e42c886e23bb4a85c61831
Sam Neisewander <samuelneisewander@gmail.com>
2025-04-14 20:17:30 -0400

    Fix: Light mode missing styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
       <div className='min-w-2xl w-1/2 h-full p-5 flex flex-col rounded-lg'>
         <div className='h-30'></div>
         <ContactCard headshot={samNeisewanderHeadshot}></ContactCard>
-        {/* <h1 className='text-stone-600 dark:text-stone-300'>Heading 1</h1>
-        <h2 className='text-stone-600 dark:text-stone-300'>Heading 2</h2>
-        <h3 className='text-stone-600 dark:text-stone-300'>Heading 3</h3>
-        <h4 className='text-stone-600 dark:text-stone-300'>Heading 4</h4>
-        <p className='text-stone-600 dark:text-stone-300'>Paragraph</p> */}
+        {/* <h1 className='text-stone-800 dark:text-stone-300'>Heading 1</h1>
+        <h2 className='text-stone-800 dark:text-stone-300'>Heading 2</h2>
+        <h3 className='text-stone-800 dark:text-stone-300'>Heading 3</h3>
+        <h4 className='text-stone-800 dark:text-stone-300'>Heading 4</h4>
+        <p className='text-stone-800 dark:text-stone-300'>Paragraph</p> */}
       </div>
     </div>
   )
@@ -23,16 +23,16 @@ function ContactCard({ headshot }: { headshot: string }){
     <>
       <div className="flex flex-col gap-3 items-center">
         <img src={headshot} className="size-40 rounded-4xl" alt="Headshot of Sam Neisewander" />
-        <h3 className='text-stone-600 dark:text-stone-300'>Sam Neisewander</h3>
+        <h3 className='text-stone-800 dark:text-stone-300'>Sam Neisewander</h3>
         <div className='flex flex-row gap-3'>
           <a href="https://www.linkedin.com/in/samuelneisewander" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} className='text-stone-100 text-2xl'></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faLinkedin} className='text-2xl text-stone-800 dark:text-stone-100'></FontAwesomeIcon>
           </a>
           <a href="https://www.instagram.com/samneisewander" target="_blank">
-            <FontAwesomeIcon icon={faInstagram} className='text-stone-100 text-2xl'></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faInstagram} className='text-2xl text-stone-800 dark:text-stone-100'></FontAwesomeIcon>
           </a>
           <a href="" target="_blank">
-            <FontAwesomeIcon icon={faSpotify} className='text-stone-100 text-2xl'></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faSpotify} className='text-2xl text-stone-800 dark:text-stone-100'></FontAwesomeIcon>
           </a>
 
         </div>
